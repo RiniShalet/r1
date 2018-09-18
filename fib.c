788b409a2f665afa5beba730323cfdcf267c0145
RiniShalet <43372311+RiniShalet@users.noreply.github.com>
2018-09-18 16:22:23 +0530

    Update fib.c

--- fib.c
+++ fib.c
@@ -5,7 +5,7 @@ int main()
     int i,n,t1=0,t2=1,nt;
     printf("enter  n \n");
     scanf("%d",&n);
-    printf("finonacci series for n numbers\n");
+    printf("fibonacci series for n numbers\n");
     for(i=0;i<n;i++)
     {
         if(i<=1)
@@ -16,7 +16,7 @@ int main()
         t1=t2;
         t2=nt;
     }
-    printf("%d\n",nt);
+    printf("fibonacci series : %d\n",nt);
     }
     return 0;
 }
